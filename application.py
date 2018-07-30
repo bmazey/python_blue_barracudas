3a242bc8da54a89f36f16a27a1ab5655d50f7c63
Ferennie <ferennie.liu0119@gmail.com>
2018-07-30 16:04:49 -0400

    trying things out

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restplus import Resource, Api
 
-
+# what's happening
 # welcome to flask: http://flask.pocoo.org/
 # working with sqlalchemy & swagger:
 # http://michal.karzynski.pl/blog/2016/06/19/building-beautiful-restful-apis-using-flask-swagger-ui-flask-restplus/
@@ -12,7 +12,7 @@ api = Api(application)
 @api.route("/hello")                   # Create a URL route to this resource
 class HelloWorld(Resource):            # Create a RESTful resource
     def get(self):                     # Create GET endpoint
-        return {'hello': 'world'}
+        return {'what\'s': 'good'}
 
 
 def main():
