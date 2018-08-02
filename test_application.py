12c9691126ffc608cb1caf8e09b53b96eb9981e3
Ferennie <ferennie.liu0119@gmail.com>
2018-08-02 15:24:40 -0400

    removed json responses

--- test_application.py
+++ test_application.py
@@ -10,59 +10,49 @@ class AppDBTests(TestCase):
     # tests get all items
     def test_simple_get(self):
         response = self.client.get('/items')
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests getting item with corresponding id
     def test_id_get(self):
         response = self.client.get("/items/id/<int:id>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests getting item with corresponding name
     def test_name_get(self):
         response = self.client.get("/items/name/<string:name>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests getting item with corresponding price
     def test_price_get(self):
         response = self.client.get("/items/price/<int:price>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests getting item with corresponding size
     def test_size_get(self):
         response = self.client.get("/items/size/<int:size>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests getting item with corresponding color
     def test_color_get(self):
         response = self.client.get("/items/color/<string:color>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests getting item with corresponding availability
     def test_availability_get(self):
         response = self.client.get("/items/availability/<string:availability>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests if item is deleted
     def test_delete(self):
         response = self.client.get("/items/delete/<int:id>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests if description is updated
     def test_patch(self):
         response = self.client.get("/items/delete/<int:id>/<string:new_description>")
-        print(response.get_json())
         self.assertTrue(200, response.status_code)
 
     # tests 418 teapot error
     def test_teapot(self):
         response = self.client.get("/teapot")
-        print(response.get_json())
         self.assertTrue(418, response.status_code)
